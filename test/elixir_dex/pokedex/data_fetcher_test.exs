85b57eaeb5c17756a1076701e63e55ab60a3cd62
Samuel Yeo <yeosamuel95@gmail.com>
2022-02-27 04:20:12 -0500

    chore: update data fetcher test

--- test/elixir_dex/pokedex/data_fetcher_test.exs
+++ test/elixir_dex/pokedex/data_fetcher_test.exs
@@ -7,10 +7,40 @@ defmodule ElixirDex.Pokedex.DataFetcherTest do
 
   setup do
     mock(fn
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/ditto"} ->
+        %Tesla.Env{status: 200, body: %{"name" => "dittoe", "id" => "1"}}
 
+        %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/1"} ->
+          %Tesla.Env{status: 200, body: %{"name" => "dittoe", "id" => "1"}}
+
+        %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/dottoe"} ->
+          %Tesla.Env{status: 404}
+
+        %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/10000"} ->
+          %Tesla.Env{status: 404}
     end)
 
     :ok
   end
 
+  describe "pokemon_name_or_id/2" do
+    test "check happy paths" do
+      fake_ditto_id = "1"
+      fake_ditto_name = "dittoe"
+      client = DataFetcher.client()
+
+      assert DataFetcher.pokemon_name_or_id(client, "ditto") == fake_ditto_id
+      assert DataFetcher.pokemon_name_or_id(client, "1") == fake_ditto_name
+    end
+
+    test "check sad paths" do
+      bad_ditto_id = "10000"
+      bad_ditto_name = "dottoe"
+      expected_error = "Not Found"
+      client = DataFetcher.client()
+
+      assert DataFetcher.pokemon_name_or_id(client, bad_ditto_name) == expected_error
+      assert DataFetcher.pokemon_name_or_id(client, bad_ditto_id) == expected_error
+    end
+  end
 end
