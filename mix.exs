d3e4ab0f901bcd6a7a7038281e3a18affb5c3c43
Samuel Yeo <yeosamuel95@gmail.com>
2022-02-27 05:02:43 -0500

    chore: add deps credo ➕

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule ElixirDex.MixProject do
       {:tesla, "~> 1.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
