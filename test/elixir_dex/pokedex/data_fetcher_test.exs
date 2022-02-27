f28a3f1a096196240a6fcbd8d04d928649a80a3c
Samuel Yeo <yeosamuel95@gmail.com>
2022-02-27 04:44:29 -0500

    chore: update test

--- test/elixir_dex/pokedex/data_fetcher_test.exs
+++ test/elixir_dex/pokedex/data_fetcher_test.exs
@@ -23,6 +23,19 @@ defmodule ElixirDex.Pokedex.DataFetcherTest do
     :ok
   end
 
+  describe "pokemon_api_response/2" do
+    test "receive entire http response" do
+      client = DataFetcher.client()
+
+      assert {:ok, %Tesla.Env{} = env} = DataFetcher.pokemon_api_response(client, "ditto")
+      assert env.status == 200
+      assert env.body == %{"name" => "dittoe", "id" => "1"}
+
+      assert {:ok, %Tesla.Env{} = env} = DataFetcher.pokemon_api_response(client, "10000")
+      assert env.status == 404
+    end
+  end
+
   describe "pokemon_name_or_id/2" do
     test "check happy paths" do
       fake_ditto_id = "1"
@@ -41,6 +54,30 @@ defmodule ElixirDex.Pokedex.DataFetcherTest do
 
       assert DataFetcher.pokemon_name_or_id(client, bad_ditto_name) == expected_error
       assert DataFetcher.pokemon_name_or_id(client, bad_ditto_id) == expected_error
+    end
+  end
+
+  describe "is_id?/1" do
+    test "decides if input is an id or not based on how input is Integer.parse'd." do
+      id = "132"
+      name = "ditto"
+      mixed_id_name_1 = "123ditto"
+      mixed_id_name_2 = "123 ditto"
+
+      assert DataFetcher.is_id?(id)
+      refute DataFetcher.is_id?(name)
+      refute DataFetcher.is_id?(mixed_id_name_1)
+      refute DataFetcher.is_id?(mixed_id_name_2)
+    end
+  end
+
+  describe "choose_name_or_id/3" do
+    test "will decide to return a name or id based on if input is considered id or name" do
+      id = "132"
+      name = "ditto"
+
+      assert DataFetcher.choose_name_or_id(id, name, id) == name
+      assert DataFetcher.choose_name_or_id(id, name, name) == id
     end
   end
 end
