aa6ca9a9d983c20551cc19eb68d120f85888cc4c
Samuel Yeo <yeosamuel95@gmail.com>
2022-02-26 21:16:26 -0500

    chore: create barebones module files for Pokedex context

---
+++ lib/elixir_dex/pokedex/data_cleaner.ex
@@ -0,0 +1,8 @@
+defmodule ElixirDex.Pokedex.DataCleaner do
+  @moduledoc """
+  Inputs and outputs should be cleaned as we expect Trainers not to input
+  names and ID's perfectly every single use. Trainer may be in tense situations
+  such as running away from strong Pokemon, or in an icy cave and have other
+  concerns outside of capitalizing and spacing characters.
+  """
+end
---
+++ lib/elixir_dex/pokedex/data_fetcher.ex
@@ -0,0 +1,8 @@
+defmodule ElixirDex.Pokedex.DataFetcher do
+  @moduledoc """
+  A Pokedex must be able to fetch data from some sort of central database. This
+  module handles situations such that the Trainer is in a forest, ocean, etc. and
+  can scan a Pokemon by ID or Name at any given moment, and be able to receive data
+  about inputted Pokemon.
+  """
+end
